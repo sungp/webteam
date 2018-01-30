c2565d2a64b3aa9f7c86289ccdc672bf7ac956a0
Sung <sungp2k@gmail.com>
2018-01-30 06:18:19 -0800

    made copy of flask webserver

---
+++ vagrant/webserver-team8-flask-db.py
@@ -0,0 +1,94 @@
+
+from flask import Flask, render_template, request, redirect, jsonify,\
+        url_for, flash
+import cgi
+
+app = Flask(__name__)
+
+@app.route('/')
+def showLink():
+  htmlText = render_template('main.html');
+  return htmlText; 
+
+@app.route('/hello', methods=['GET', 'POST'])
+def showHello():
+  submitMsg = "";
+  
+  if request.method == 'POST':
+    submitMsg = request.form['message'].strip();
+  
+  htmlText = render_template('hello.html', request = request, message = submitMsg);  
+  print htmlText;
+  return htmlText;
+
+@app.route('/happy123', methods=['GET', 'POST'])
+def showHappy123():
+  message = "";
+  landingpage = "";
+
+  if request.method == 'POST':
+    dictval = request.form.to_dict()
+    print dictval
+    message = dictval['message']
+    landingpage = dictval['landingpage']
+    write_file(message, landingpage)
+
+  htmlText = render_template('happy123.html', request = request, message = message, landingpage = landingpage);  
+  print htmlText;
+  return htmlText;
+
+@app.route('/gloomy321', methods=['GET', 'POST'])
+def showGloomy321():
+  message = "";
+  landingpage = "";
+
+  if request.method == 'POST':
+    dictval = request.form.to_dict()
+    print dictval
+    message = dictval['message']
+    landingpage = dictval['landingpage']
+    write_file(message, landingpage)
+
+  htmlText = render_template('gloomy321.html', request = request, message = message, landingpage = landingpage);  
+  print htmlText;
+  return htmlText;
+
+@app.route('/robobook', methods=['GET'])
+def showRoboBook():
+  content = ""
+  content = read_file()
+
+  htmlText = render_template('robobook.html', request = request, content = content);  
+  print htmlText;
+  return htmlText;
+
+def write_file(message, landingpagecontent):
+    print 'user printed ' + message
+    fh = open("messages.txt","a")
+    fh.write(message + " , " + landingpagecontent)
+    fh.write('\n')
+    fh.close()
+
+def read_file():
+    fh = open("messages.txt","r")
+    lines = fh.readlines()
+    print lines
+
+    content = {}
+    user = ""
+    url = ""
+
+    for line in lines:
+      data = line.split(",") 
+      user = data[0].strip()
+      url = data[1].strip()
+
+      content[user] = url
+
+    fh.close()
+    return content
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8080)
