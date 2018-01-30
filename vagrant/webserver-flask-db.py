430dfad6a8ab9cd7cdc01f0a67d84d54ac42bf85
Sung <sungp2k@gmail.com>
2018-01-30 07:11:02 -0800

    post page works

--- vagrant/webserver-flask-db.py
+++ vagrant/webserver-flask-db.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, request, redirect, jsonify,\
         url_for, flash
-import cgi
+from functools import wraps
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Group, Post 
 
 app = Flask(__name__)
 
+# Connect to Database and create database session
+engine = create_engine('sqlite:///grouppost.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 @app.route('/')
 def showLink():
-  htmlText = render_template('post.html');
+  postsOrdered = session.query(Post).order_by(asc(Post.date_added))
+  htmlText = render_template('post.html', posts = postsOrdered)
   return htmlText; 
 
 @app.route('/hello', methods=['GET', 'POST'])
@@ -14,9 +25,9 @@ def showHello():
   submitMsg = "";
   
   if request.method == 'POST':
-    submitMsg = request.form['message'].strip();
+    submitMsg = request.form['message'].strip()
   
-  htmlText = render_template('hello.html', request = request, message = submitMsg);  
+  htmlText = render_template('hello.html', request = request, message = submitMsg) 
   print htmlText;
   return htmlText;
 
